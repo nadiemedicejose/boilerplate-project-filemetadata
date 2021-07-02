583f892974d0735047d0eb55a13e6ce00e4566ee
Jose Estrada <j.estrada49@icloud.com>
2021-07-02 07:55:29 -0700

    const over vars, and arrow functions

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
-const multer = require('multer');
-var app = express();
+const multer = require('multer')
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
-
-
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/views/index.html')
+})
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
-});
+})
