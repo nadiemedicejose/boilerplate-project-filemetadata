e3a0a838ec1f226f5409b40b0aab54247158f101
Jose Estrada <j.estrada49@icloud.com>
2021-07-02 08:05:52 -0700

    submit a form that includes a file upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const multer = require('multer')
+const upload = multer({})
 const app = express()
 
 app.use(cors())
@@ -11,8 +12,20 @@ app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/views/index.html')
 })
 
-
-
+/**
+ * You can submit a form that includes a file upload.
+ * The form file input field has the name attribute set to 'upfile'.
+ * When you submit a file, you receive the file name, type, and size
+ * in bytes within the JSON response.
+ */
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
